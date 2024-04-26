ee804e924eefef9089e3ad19b0901318fdf09f8b
Renuka <renu.bagave1901@gmail.com>
2024-04-26 13:25:50 +0530

    [Renuka]Add : Added queue with all methods

---
+++ Queue/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of queue to be created");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Queue q = new Queue();
+            q.CreateQueue(size);
+            int options;
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Enqueue : 1");
+                Console.WriteLine("Dequeue : 2");
+                Console.WriteLine("Peek : 3");
+                Console.WriteLine("Contains : 4");
+                Console.WriteLine("Count : 5");
+                Console.WriteLine("Clear : 6");
+                Console.WriteLine("Print : 7");
+                Console.WriteLine("Exit : 0");
+
+                options = Convert.ToInt32(Console.ReadLine());
+
+                switch (options)
+                {
+                    case 1:
+                        if (!q.IsFull())
+                        {
+                            Console.WriteLine("Enter the number  : ");
+                            int no = Convert.ToInt32(Console.ReadLine());
+                            q.Enqueue(no);
+
+                        }
+                        else
+                            Console.WriteLine("Queue is full");
+                        break;
+
+                    case 2:
+                        if (!q.IsEmpty())
+                        {
+                            q.Dequeue();
+                            Console.WriteLine($"Element Deleted from queue");
+                        }
+                            
+                        else
+                            Console.WriteLine("Queue is Empty");
+                        break;
+
+                    case 3:
+                        q.Peek();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter value to be searched");
+                        int value = Convert.ToInt32(Console.ReadLine());
+                        if (q.Contains(value))
+                            Console.WriteLine("Element Found");
+                        else
+                            Console.WriteLine("Element Not Found");
+                        break;
+
+                    case 5:
+                        q.Count();
+                        break;
+
+                    case 6:
+                        q.Clear();
+                        Console.WriteLine("All Elements are Deleted");
+                        break;
+
+                    case 7:
+                        if (!q.IsEmpty())
+                            q.PrintQueue();
+                        else
+                            Console.WriteLine("Queue is empty");
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+            } while (options != 0);
+            
+        }
+            
+            
+            
+            
+        }
+    }
+
---
+++ Queue/Queue.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Queue
+{
+    public class Queue
+    {
+        int Rear, Front, MaxSize, count;
+        int[] queue;
+
+        public void CreateQueue(int size)
+        {
+            Rear = -1;
+            Front = 0;
+            MaxSize = size;
+            queue = new int[size];
+            count = 0;
+            Console.WriteLine("Queue Created");
+        }
+
+        public void Enqueue(int number)
+        {
+            Rear++;
+            count++;
+            queue[Rear] = number;
+            Console.WriteLine("Element Added to queue");
+        }
+
+        public void Dequeue()
+        {
+            int temp = queue[Front];
+            Front++;
+            count--;
+        }
+
+        public void PrintQueue() 
+        {
+            for(int i = Front; i <= Rear; i++)
+            {
+                Console.Write(queue[i] + " ");
+            }
+        }
+
+        public void Peek()
+        {
+            Console.WriteLine($"Element at peek is {queue[Front]}");
+        }
+
+        public bool Contains(int value)
+        {
+            bool flag = false;
+            for (int i = Front; i <= Rear; i++)
+            {
+                if (queue[i] == value)
+                    flag = true;
+
+            }
+            if(flag == true)
+                return true;
+            else
+                return false;
+        }
+
+        public void Clear()
+        {
+            while (Rear >= Front)
+            {
+                Front++;
+                count--;
+            }
+        }
+
+        public void Count()
+        {
+            Console.WriteLine($"Total number of elements : {count}");
+        }
+        public bool IsFull()
+        {
+            if(Rear == MaxSize-1)
+                return true;
+            else 
+                return false;
+                
+        }
+        public bool IsEmpty()
+        {
+            if(Front > Rear) 
+                return true;
+            else 
+                return false;
+        }
+    }
+}
